10ddcfcecb01ea2009c6fc23990a550c55a61f79
mj <mj@Matthews-MacBook-Pro.local>
2016-07-18 04:18:00 -0400

    Initial commit

---
+++ tcp_client.py
@@ -0,0 +1,18 @@
+import socket, base64
+
+target_host = "0.0.0.0"
+target_port = 9999
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(base64.b64encode(b"Secret Code is babalbalababalblaba\r\n\r\n"))
+
+# receive data
+response = client.recv(4096)
+
+print response
